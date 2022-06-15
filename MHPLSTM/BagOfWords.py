5c3556c02e203c5dd681f14ceb27992d5db7a5fe
daitran2k1 <tranquangdai7a@gmail.com>
2022-06-15 11:07:29 +0700

    upload 2 modules of MHPLSTM

--- MHPLSTM/BagOfWords.py
+++ MHPLSTM/BagOfWords.py
@@ -1,8 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import torch
 import torch.nn as nn
-import re
-import numpy as np
 
 
 def build_vectorizer_and_vocab(sentences):
@@ -15,36 +13,54 @@ def build_vectorizer_and_vocab(sentences):
     return vectorizer, vocab, vocab_size
 
 
-def bag_of_words_presentation(sentence, vectorizer, vocab, vocab_size):
+def bag_of_words_presentation(sentence, vectorizer, vocab_size):
+    i_vectors = []
+    s_vectors = []
+    s_ith = torch.zeros(1, vocab_size)
+    s_vectors.append(s_ith)
     sentence = sentence.split()
 
-    outputs = [0] * vocab_size
-    input_step_0 = re.sub(r'[^\w\s]', '', sentence[0]).lower()
+    for i in range(len(sentence)):
+        input = [sentence[i]]
+        input_ith = torch.tensor(vectorizer.transform(input).toarray(), dtype=torch.float32)
+        i_vectors.append(input_ith)
+        if (i != 0):
+            s_ith = torch.add(s_ith, i_vectors[i-1])
+            s_vectors.append(s_ith)
 
-    if input_step_0 in vocab:
-        input_step_0_idx = vocab[input_step_0]
-    else:
-        input_step_0_idx = len(vocab)
+    return i_vectors, s_vectors
+
+
+# def bag_of_words_presentation(sentence, vectorizer, vocab, vocab_size):
+#     sentence = sentence.split()
+
+#     outputs = [0] * vocab_size
+#     input_step_0 = re.sub(r'[^\w\s]', '', sentence[0]).lower()
+
+#     if input_step_0 in vocab:
+#         input_step_0_idx = vocab[input_step_0]
+#     else:
+#         input_step_0_idx = len(vocab)
         
-    outputs.append(float(input_step_0_idx))
-    outputs = torch.tensor(outputs).reshape((1, vocab_size + 1))
+#     outputs.append(float(input_step_0_idx))
+#     outputs = torch.tensor(outputs).reshape((1, vocab_size + 1))
 
-    for i in range(1, len(sentence)):
-        input = [' '.join(sentence[:i])]
-        output = vectorizer.transform(input).toarray()
-        input_step_t = sentence[i]
-        input_step_t = re.sub(r'[^\w\s]', '', input_step_t).lower()
+#     for i in range(1, len(sentence)):
+#         input = [' '.join(sentence[:i])]
+#         output = vectorizer.transform(input).toarray()
+#         input_step_t = sentence[i]
+#         input_step_t = re.sub(r'[^\w\s]', '', input_step_t).lower()
 
-        if input_step_t in vocab:
-            input_step_t_idx = vocab[input_step_t]
-        else:
-            input_step_t_idx = len(vocab)
+#         if input_step_t in vocab:
+#             input_step_t_idx = vocab[input_step_t]
+#         else:
+#             input_step_t_idx = len(vocab)
 
-        output = np.append(output, float(input_step_t_idx))
-        output = torch.tensor(output).reshape((1, vocab_size + 1))
-        outputs = torch.cat((outputs, output), dim=0)
+#         output = np.append(output, float(input_step_t_idx))
+#         output = torch.tensor(output).reshape((1, vocab_size + 1))
+#         outputs = torch.cat((outputs, output), dim=0)
 
-    return outputs
+#     return outputs
 
 
 class BagOfWord(nn.Module):
@@ -54,4 +70,4 @@ class BagOfWord(nn.Module):
         self.vectorizer, self.vocab, self.vocab_size = build_vectorizer_and_vocab(sentences)
 
     def forward(self, input):
-        return bag_of_words_presentation(input, self.vectorizer, self.vocab, self.vocab_size)
+        return bag_of_words_presentation(input, self.vectorizer, self.vocab_size)
--- MHPLSTM/Modules.py
+++ MHPLSTM/Modules.py
@@ -4,3 +4,79 @@ from torch.autograd import Variable
 import numpy as np
 
 
+class FirstModule(nn.Module):
+    """Input gate + forget gate + hidden state"""
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(FirstModule, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        self.ifh1 = nn.Linear(input_size, hidden_size * 3, bias=bias)
+        self.h2 = nn.Linear(hidden_size, hidden_size, bias=bias)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1.0 / np.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, inputs):
+        """
+            input = (i_vectors, s_vectors)
+            i_vectors.shape: (seq_length, input_size / 2)
+            s_vectors.shape: (seq_length, input_size / 2)
+        """
+
+        i_vectors, s_vectors = inputs
+        v_vectors = []
+        for i in range(len(i_vectors)):
+            v_vectors.append(torch.cat((i_vectors[i], s_vectors[i]), dim=0))
+        v = torch.stack([v_vector.squeeze() for v_vector in v_vectors])
+
+        gates = self.ifh1(v)
+        ig, fg, h = gates.chunk(3, 1)
+
+        ig = torch.sigmoid(ig)
+        fg = torch.sigmoid(fg)
+        h = self.h2(torch.tanh(h))
+        hr = h * ig
+
+        return fg, hr
+
+
+class SecondModule(nn.Module):
+    """output gate"""
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(SecondModule, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        self.o = nn.Linear(input_size, hidden_size, bias=bias)
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        std = 1.0 / np.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, inputs):
+        """"
+            inputs = (i_vectors, c_vectors)
+            i_vectors.shape: (seq_length, input_size / 2)
+            c_vectors.shape: (seq_length, input_size / 2)
+            
+        """
+        
+        i_vectors, c_vectors = inputs
+        ic_vectors = []
+        for i in range(len(i_vectors)):
+            ic_vectors.append(torch.cat((i_vectors[i], c_vectors[i]), dim=0))
+        ic = torch.stack([ic_vector.squeeze() for ic_vector in ic_vectors])
+        c = torch.stack([c_vector.squeeze() for c_vector in c_vectors])
+
+        og = torch.sigmoid(self.o(ic))
+        o = c * og
+
+        return o
